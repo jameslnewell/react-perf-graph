188b137c4beb59d973a5ad5860134e0750dac691
James Newell <james@digitaledgeit.com.au>
2016-10-19 12:12:33 +1100

    printing the legened manually

--- src/components/ActivityGraph/index.js
+++ src/components/ActivityGraph/index.js
@@ -4,7 +4,8 @@ import Helmet from 'react-helmet';
 import randomcolor from 'randomcolor';
 import {getSelectedActivity} from '../../reducer';
 import {addActivityStats} from '../../actions';
-import {LineChart, XAxis, YAxis, CartesianGrid, Legend, Line} from 'recharts';
+import {LineChart, XAxis, YAxis, CartesianGrid, Line} from 'recharts';
+import './index.css';
 
 const mapStateToProps = (state, props) => ({
   activity: getSelectedActivity(state)
@@ -36,8 +37,11 @@ class ActivityGraph extends React.Component {
       return [];
     }
 
-    return this.props.activity.changes[0].stats.map(stat => stat['Owner > Component'])
-      .filter((item, i) => i<10)
+    return this.props.activity.changes[0].stats.map(stat => ({
+      label: stat['Owner > Component'],
+      color: randomcolor()
+    }))
+      // .filter((item, i) => i<10)
     ;
   }
 
@@ -66,20 +70,34 @@ class ActivityGraph extends React.Component {
       return <p>No activity is selected... Select an activity to view the change over time.</p>
     }
 
+    const lines = this.getGraphLines();
+
     return (
-      <div className="activity-graph">
+      <div className="graph-content" style={{display: 'flex', 'flexDirection': 'column', height: '100%'}}>
         <Helmet title={activity.name}/>
         <h4>{activity.name}</h4>
         <h5>Render count</h5>
 
-        <LineChart width={600} height={600} margin={{top: 5, right: 30, left: 20, bottom: 5}} data={this.getGraphData()}>
-          <XAxis dataKey="change"/>
-          <YAxis/>
-          <CartesianGrid strokeDasharray="6 6"/>
-          {/* TODO: how to get the tooltip just showing the name of the line <Tooltip content={<CustomTooltip/>}/> */}
-          <Legend />
-          {this.getGraphLines().map(line => <Line type="monotone" dataKey={line} key={line} stroke={randomcolor()}/>)}
-        </LineChart>
+        <div className="activity-graph" style={{flexGrow: 1}}>
+          <LineChart width={1200} height={600} data={this.getGraphData()}>
+            <XAxis dataKey="change"/>
+            <YAxis/>
+            <CartesianGrid strokeDasharray="6 6"/>
+            {/* TODO: how to get the tooltip just showing the name of the line <Tooltip content={<CustomTooltip/>}/> */}
+            {/*}<Legend />*/}
+            {lines.map(line => (
+              <Line key={line.label} dataKey={line.label} stroke={line.color} type="monotone"/>  
+            ))}
+          </LineChart>
+        </div>
+
+        <div className="graph-legend">
+          {lines.map(line => (
+            <label className="graph-legend__line" style={{color: line.color}}>
+              {line.label}
+            </label>
+          ))}
+        </div>
 
         <button onClick={this.handleAddStats.bind(this)}>+ Add stats</button>
 
--- src/components/App.js
+++ src/components/App.js
@@ -9,12 +9,22 @@ class App extends Component {
   render() {
     return (
       <div>
-        <header className="site-header">react-perf-graph</header>
+        <header className="site-header">
+          <span className="site-header__title">
+            react-perf-graph
+          </span>
+          <span className="site-header__badge">
+            <a className="github-button" href="https://github.com/jameslnewell/react-perf-graph/fork" aria-label="Fork jameslnewell/react-perf-graph on GitHub">
+              Fork
+            </a>
+          </span>
+        </header>
         <Helmet titleTemplate="react-perf-graph - %s" title="Graph the results of `react-addons-perf`."/>
         <TwoColumnLayout>
           <ActivityList/>
           <ActivityGraph/>
         </TwoColumnLayout>
+        <script async defer src="https://buttons.github.io/buttons.js"></script>
       </div>
     );
   }
